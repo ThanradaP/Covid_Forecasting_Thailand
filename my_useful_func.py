5420b1192a9b7bebb853fad1d89b81f0325281d5
Thanrada Popraros <79976554+ThanradaP@users.noreply.github.com>
2022-05-19 16:36:45 +0700

    Update my_useful_func.py

--- my_useful_func.py
+++ my_useful_func.py
@@ -1,4 +1,4 @@
-# from turtle import color, fillcolor
+from turtle import color, fillcolor
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -58,7 +58,7 @@ def timeseries_evaluation_metrics_func(y_true, y_pred):
         
 @st.cache(suppress_st_warning=True)      
 def find_orders(ts,periods_input):
-    stepwise_model = auto_arima(ts, start_p=1, start_q=1,
+    stepwise_model = auto_arima(ts.dropna(), start_p=1, start_q=1,
                       test='adf',       # use adftest to find optimal 'd'
                       max_p=5, max_q=5, # maximum p and q 
                       # m=7, # daily data   
@@ -70,7 +70,7 @@ def find_orders(ts,periods_input):
                       error_action='ignore',  
                       suppress_warnings=True, 
                       stepwise=True) # this works 
-    fitted = stepwise_model.predict(n_periods=periods_input)
+    fitted = stepwise_model.predict(n_periods=periods_input+7)
 
     return fitted
 
@@ -95,73 +95,55 @@ def cases_deaths_plot(df):
 
 def exog_plot(df):
     exog = df[['new_cases','new_deaths','full_vac','resident_gg']].copy()
-    fig = make_subplots(rows=4, subplot_titles=('new_cases','new_deaths','full_vac','resident_gg'))
-    fig.add_scatter(x=exog.index, y=exog.new_cases, row=1, col=1, name='new_cases') 
-    fig.add_scatter(x=exog.index, y=exog.new_deaths, row=2, col=1, name='new_deaths') 
-    fig.add_scatter(x=exog.index, y=exog.full_vac, row=3, col=1,name='full_vac') 
-    fig.add_scatter(x=exog.index, y=exog.resident_gg, row=4, col=1,name='resident_gg') 
+    fig = make_subplots(rows=2, subplot_titles=('New deaths','People fully vacinated'))
+    # fig.add_scatter(x=exog.index, y=exog.new_cases, row=1, col=1, name='new_cases') 
+    fig.add_scatter(x=exog.index, y=exog.new_deaths, row=1, col=1, name='new_deaths') 
+    fig.add_scatter(x=exog.index, y=exog.full_vac, row=2, col=1,name='full_vac') 
+    # fig.add_scatter(x=exog.index, y=exog.resident_gg, row=4, col=1,name='resident_gg') 
     fig.update_layout(height=800, width=800,title_text='Exogenous variable plot')
 
     with st.expander("Exogenous variables"):
         st.write("""
         The exogenous variable is a parallel time series that are not modeled directly but is used as a weighted input to the model. 
-        This project has selected 4 external variables as follows: \n
-            * New cases: Daily number of new infections \n
+        This project has selected 2 external variables as follows: \n
             * Deaths: Daily death \n
             * Full vac: Number of people who have completed two doses of vaccination \n
-            * Resident_gg: residential_percent_change_from_baseline \n
         """)
         st.plotly_chart(fig)
-        
-# def myplot(train, test, predict_on_test, forecast_future):
-#     fig = plt.figure()
-#     plt.rcParams["figure.figsize"] = [15,7]
-#     plt.plot(train, label='Train ')
-#     plt.plot(test, label='Test ')
-#     plt.plot(predict_on_test, label='Predict on test set')
-#     plt.plot(forecast_future.index,forecast_future['pred'], label='Predicted ')
-#     plt.plot(forecast_future.index,forecast_future['upper'], label='Confidence Interval Upper bound ')
-#     plt.plot(forecast_future.index,forecast_future['lower'], label='Confidence Interval Lower bound ')
-#     plt.axvspan(predict_on_test.index[0],predict_on_test.index[-1], color='pink', alpha=0.5)
-#     plt.axvspan(forecast_future.index[0],forecast_future.index[-1], color='yellow', alpha=0.5)
-#     plt.legend(loc='best')
-#     return fig
+    
 
-def lstm_plot(train, test, predict_on_test, predict_future):
+def lstm_plot(df, predict_future):
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=train.index, y=train.rolling7,
+    fig.add_trace(go.Scatter(x=df.index, y=df.new_cases_rolling7,
                         mode='lines',
                         name='train'))
-    fig.add_trace(go.Scatter(x=test.index, y=test.rolling7,
-                        mode='lines',
-                        name='test'))
-    fig.add_trace(go.Scatter(x=predict_on_test.index, y=predict_on_test['pred'],
-                        mode='lines',
-                        name='predict on test set'))
     fig.add_trace(go.Scatter(x=predict_future.index, y=predict_future['pred'],
                         mode='lines',
                         name='predict future'))
-    fig.update_xaxes(range=[test.index[-30],predict_future.index[-1]],
+    fig.update_xaxes(range=[df.index[-30],predict_future.index[-1]],
         rangeselector_bgcolor= '#6C6665',
-    rangeslider_visible=True,
-    rangeselector=dict(
-        buttons=list([
-            dict(count=1, label="1m", step="month", stepmode="backward"),
-            dict(count=6, label="6m", step="month", stepmode="backward"),
-            dict(count=1, label="YTD", step="year", stepmode="todate"),
-            dict(count=1, label="1y", step="year", stepmode="backward"),
-            dict(step="all")
-        ])))
+        rangeslider_visible=True,
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1, label="1m", step="month", stepmode="backward"),
+                dict(count=6, label="6m", step="month", stepmode="backward"),
+                dict(count=1, label="YTD", step="year", stepmode="todate"),
+                dict(count=1, label="1y", step="year", stepmode="backward"),
+                dict(step="all")
+            ])))
     return fig
 
-def arima_plot(train,test,predict_future): #predict_on_test
+def arima_plot(df,predict_future): #predict_on_test
   fig = go.Figure()
-  fig.add_trace(go.Scatter(x=train.index, y=train.new_cases_rolling7,
+  fig.add_trace(go.Scatter(x=df.index, y=df['new_cases_rolling7'],
                       mode='lines',
-                      name='train'))
-  fig.add_trace(go.Scatter(x=test.index, y=test.new_cases_rolling7,
-                      mode='lines',
-                      name='test'))
+                      name='Actual data'))
+#   fig.add_trace(go.Scatter(x=train.index, y=train.new_cases_rolling7,
+#                       mode='lines',
+#                       name='train'))
+#   fig.add_trace(go.Scatter(x=test.index, y=test.new_cases_rolling7,
+#                       mode='lines',
+#                       name='test'))
 #   fig.add_trace(go.Scatter(x=test.index, y=predict_on_test['pred'],
 #                       mode='lines',
 #                       name='predict on test set'))
@@ -170,10 +152,10 @@ def arima_plot(train,test,predict_future): #predict_on_test
                       name='predict future'))
   fig.add_trace(go.Scatter(x=predict_future.index, y=predict_future['lower'],
                       mode='lines',
-                      name='lower'))
+                      name='Lower bounds of the 95% confidence interval'))
   fig.add_trace(go.Scatter(x=predict_future.index, y=predict_future['upper'],
                       mode='lines',
-                      name='upper'))
+                      name='Upper bounds of the 95% confidence interval'))
 #   fig.add_vrect(
 #       x0=test.index[0], x1=test.index[-1],
 #       fillcolor="LightPink", opacity=0.5,
@@ -184,7 +166,7 @@ def arima_plot(train,test,predict_future): #predict_on_test
       fillcolor="Yellow", opacity=0.5,
       layer="below", line_width=0,
       )
-  fig.update_xaxes(range=[test.index[-30],predict_future.index[-1]],
+  fig.update_xaxes(range=[df.index[-30],predict_future.index[-1]],
       rangeselector_bgcolor= '#6C6665',
     rangeslider_visible=True,
     rangeselector=dict(
@@ -197,26 +179,29 @@ def arima_plot(train,test,predict_future): #predict_on_test
         ])))
   return fig
 
-def prophet_plot(train,test,predict_future):
+def prophet_plot(prophet_df,predict_future):
   fig = go.Figure()
-  fig.add_trace(go.Scatter(x=train.ds, y=train.y,
+  fig.add_trace(go.Scatter(x=prophet_df.ds, y=prophet_df['y'],
                       mode='lines',
-                      name='train'))
-  fig.add_trace(go.Scatter(x=test.ds, y=test.y,
-                      mode='lines',
-                      name='test'))
+                      name='Actual data'))
+#   fig.add_trace(go.Scatter(x=train.ds, y=train.y,
+#                       mode='lines',
+#                       name='train'))
+#   fig.add_trace(go.Scatter(x=test.ds, y=test.y,
+#                       mode='lines',
+#                       name='test'))
 #   fig.add_trace(go.Scatter(x=test.ds, y=predict_on_test['yhat'],
 #                       mode='lines',
 #                       name='predict on test set'))
   fig.add_trace(go.Scatter(x=predict_future.index, y=predict_future['yhat'],
                       mode='lines',
-                      name='predict future'))
+                      name='Predict future'))
   fig.add_trace(go.Scatter(x=predict_future.index, y=predict_future['yhat_lower'],
                       mode='lines',
-                      name='lower'))
+                      name='Lower bounds of the 95% confidence interval'))
   fig.add_trace(go.Scatter(x=predict_future.index, y=predict_future['yhat_upper'],
                       mode='lines',
-                      name='upper'))
+                      name='Upper bounds of the 95% confidence interval'))
 #   fig.add_vrect(
 #       x0=test.ds.iloc[0], x1=test.ds.iloc[-1],
 #       fillcolor="LightPink", opacity=0.5,
@@ -227,7 +212,7 @@ def prophet_plot(train,test,predict_future):
       fillcolor="Yellow", opacity=0.5,
       layer="below", line_width=0,
       )
-  fig.update_xaxes(range=[test.ds.iloc[-30],predict_future.index[-1]],
+  fig.update_xaxes(range=[prophet_df.ds.iloc[-30],predict_future.index[-1]],
       rangeselector_bgcolor= '#6C6665',
     rangeslider_visible=True,
     rangeselector=dict(
@@ -265,13 +250,10 @@ def get_mape(var1, var2):
 
 def simple_lstm(n_steps,n_features):
     model = Sequential()
-    model.add(LSTM(128, activation='relu', input_shape=(n_steps, n_features)))
-    model.add(Dense(64))
-    model.add(Dense(128))
-    model.add(Dense(64))
-    model.add(Dense(1))
+    model.add(LSTM(64, activation='relu', input_shape=(n_steps, n_features)))
+    model.add(Dense(7))
     model.compile(optimizer='adam', loss='mse')
-    return(model)
+    return model
 
 def simple_gru(n_steps,n_features):
     model = Sequential()
@@ -279,89 +261,77 @@ def simple_gru(n_steps,n_features):
     model.add(Dense(64))
     model.add(Dense(128))
     model.add(Dense(64))
-    model.add(Dense(1))
+    model.add(Dense(7))
     model.compile(optimizer='adam', loss='mse')
-    return(model)
+    return model
 
+# split a univariate sequence into samples
+def split_sequence(sequence, n_steps_in, n_steps_out):
+  X, y = list(), list()
+  for i in range(len(sequence)):
+    # find the end of this pattern
+    end_ix = i + n_steps_in
+    out_end_ix = end_ix + n_steps_out
+    # check if we are beyond the sequence
+    if out_end_ix > len(sequence):
+      break
+    # gather input and output parts of the pattern
+    seq_x, seq_y = sequence[i:end_ix], sequence[end_ix:out_end_ix]
+    X.append(seq_x)
+    y.append(seq_y)
+  return np.array(X), np.array(y)
 
 @st.cache(suppress_st_warning=True)
 def lstm_uni_model(data,n_periods,model_type):  #preprocess(df['new_cases'],df['rolling7']])
     n_features = 1
     n_epochs=50
     scaler = MinMaxScaler()
-    data_scaled = scaler.fit_transform(data.values.reshape(-1,1))
-    train,test = split(data_scaled)
-    # st.write(train,test)
-    # reshape into X=t,t+1,t+2,t+3 and Y=t+4
-    X_train, y_train = create_dataset(train,14)
-    X_test, y_test = create_dataset(test,14)
-    st.write(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
-    best_rmse = np.inf
-    best_mape = np.inf
-    best_lstm_model=None
-    n_steps = 14
-    params = (n_steps,n_features)
+    data_scaled = scaler.fit_transform(data.dropna().values.reshape(-1,1))
+    # choose a number of time steps
+    n_steps_in, n_steps_out = 14, 1
+    # split into samples
+    X, y = split_sequence(data_scaled, n_steps_in, n_steps_out)
+    X_train, X_test = split(X)
+    y_train, y_test = split(y)
+    X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], n_features)
+    X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], n_features)
+    n_timesteps, n_features, n_outputs = X_train.shape[1], X_train.shape[2], y_train.shape[1]
+    params = (n_steps_in,n_features)
     model = model_type(*params)
     model.fit(X_train, y_train,validation_data=(X_test,y_test), epochs=n_epochs,shuffle=False, verbose=0)
-    #one step prediction on test set
-    y_pred=[]
-    for ix in range(y_test.shape[0]): 
-        x_input = X_test[ix]
-        x_input = x_input.reshape((1, n_steps, n_features))
-        yhat = model.predict(x_input, verbose=0)
-        y_pred.extend(yhat[0])
-    
-    y_test_inv = scaler.inverse_transform(y_test.reshape(-1, 1))
-    y_pred_inv = scaler.inverse_transform(np.array(y_pred).reshape(-1,1))
-    mape = get_mape(y_test_inv, y_pred_inv)
-    st.write(str(model_type)+" -  MAPE = {:4.4f}".format(mape))
-    
-    fig = plt.figure(figsize=(16,7))
-    plt.plot(y_pred_inv,label='Pred')
-    plt.plot(y_test_inv,label='Actual')
-    plt.legend()
-    st.write(fig)
-        
-    #forecast future
-    lookback = len(test) - 14
-    x_input=test[lookback:].reshape(1,-1)
+    x_input=X_test[-1].reshape(1,-1)
     temp_input=list(x_input)
     temp_input=temp_input[0].tolist()
-    x_input=np.array(temp_input) 
+    x_input=np.array(temp_input)
     lst_output=[]
-    n_steps=14
     i=0
     while(i<n_periods):
         
-        if(len(temp_input)>14):
-            #print(temp_input)
+        if(len(temp_input)>n_timesteps):
             x_input=np.array(temp_input[1:])
             print("{} day input {}".format(i,x_input))
             x_input=x_input.reshape(1,-1)
-            x_input = x_input.reshape((1, n_steps, 1))
-            #print(x_input)
-            model = simple_lstm(n_steps,n_features)
-            history = model.fit(X_train, y_train, validation_data=(X_test,y_test), epochs = 50, verbose=0, shuffle=False)
+            x_input = x_input.reshape((1, n_timesteps, 1))
             yhat = model.predict(x_input, verbose=0)
             print("{} day output {}".format(i,yhat))
-            temp_input.extend(yhat[0].tolist())
-            temp_input=temp_input[1:] #***
-            #print(temp_input)
+            temp_input.extend([yhat[0][0]])
+            temp_input=temp_input[1:] 
             lst_output.extend(yhat.tolist())
             i=i+1
         else:
-            x_input = x_input.reshape((1, n_steps,1))
-            model = simple_lstm(n_steps,n_features)
-            history = model.fit(X_train, y_train, validation_data=(X_test,y_test), epochs = 50, verbose=0, shuffle=False)
+            x_input = x_input.reshape((1, n_timesteps,1))
+            print("{} day input {}".format(i,x_input))
             yhat = model.predict(x_input, verbose=0)
-            print(yhat[0])
-            temp_input.extend(yhat[0].tolist())
-            print(len(temp_input))
+            print("{} day output {}".format(i,yhat))
+            temp_input.extend([yhat[0][0]])
             lst_output.extend(yhat.tolist())
             i=i+1
     lst_output_inv = scaler.inverse_transform(lst_output)
-        
-    return y_pred_inv,lst_output_inv
+    pred_sum7 = []
+    for i in range(7):
+        temp = lst_output_inv[i].sum()
+        pred_sum7.append(temp)
+    return pred_sum7
 
 @st.cache(suppress_st_warning=True)    
 def future_exog(col,df,n_periods):
