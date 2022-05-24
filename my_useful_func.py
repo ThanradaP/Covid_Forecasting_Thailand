e6528e57ef7c1170b0bb95b91de14b3ba9318670
Thanrada Popraros <79976554+ThanradaP@users.noreply.github.com>
2022-05-24 14:24:34 +0700

    Update my_useful_func.py

--- my_useful_func.py
+++ my_useful_func.py
@@ -27,19 +27,10 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 import pickle
 from sklearn.metrics import mean_absolute_percentage_error
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.sequence import TimeseriesGenerator
-from sklearn.model_selection import train_test_split
 tf.random.set_seed(123)
 np.random.seed(123)
 import warnings
 warnings.filterwarnings('ignore')
-import json
-from fbprophet.diagnostics import performance_metrics
-from fbprophet.plot import plot_cross_validation_metric
-import itertools
-from fbprophet.diagnostics import cross_validation
-
 def timeseries_evaluation_metrics_func(y_true, y_pred):
     def mean_absolute_percentage_error(y_true, y_pred):
         y_true, y_pred = np.array(y_true), np.array(y_pred)
@@ -55,7 +46,8 @@ def timeseries_evaluation_metrics_func(y_true, y_pred):
         col1,col2 = st.columns(2)
         col1.metric(label='RMSE',value=round(rmse,2))
         col2.metric(label='MAPE',value=round(mape,2))
-            
+        
+    
 def find_orders(ts,periods_input):
     stepwise_model = auto_arima(ts.dropna(), start_p=1, start_q=1,
                       test='adf',       # use adftest to find optimal 'd'
@@ -95,10 +87,8 @@ def cases_deaths_plot(df):
 def exog_plot(df):
     exog = df[['new_cases','new_deaths','full_vac','resident_gg']].copy()
     fig = make_subplots(rows=2, subplot_titles=('New deaths','People fully vacinated'))
-    # fig.add_scatter(x=exog.index, y=exog.new_cases, row=1, col=1, name='new_cases') 
     fig.add_scatter(x=exog.index, y=exog.new_deaths, row=1, col=1, name='new_deaths') 
     fig.add_scatter(x=exog.index, y=exog.full_vac, row=2, col=1,name='full_vac') 
-    # fig.add_scatter(x=exog.index, y=exog.resident_gg, row=4, col=1,name='resident_gg') 
     fig.update_layout(height=800, width=800,title_text='Exogenous variable plot')
 
     with st.expander("Exogenous variables"):
@@ -113,13 +103,13 @@ def exog_plot(df):
 
 def lstm_plot(df, predict_future):
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=df.index, y=df.new_cases_rolling7,
+    fig.add_trace(go.Scatter(x=pd.to_datetime(df.date), y=df.new_cases_rolling7,
                         mode='lines',
                         name='train'))
     fig.add_trace(go.Scatter(x=predict_future.index, y=predict_future['pred'],
                         mode='lines',
                         name='predict future'))
-    fig.update_xaxes(range=[df.index[-30],predict_future.index[-1]],
+    fig.update_xaxes(range=[df.date.iloc[-30],predict_future.index[-1]],
         rangeselector_bgcolor= '#6C6665',
         rangeslider_visible=True,
         rangeselector=dict(
@@ -132,20 +122,11 @@ def lstm_plot(df, predict_future):
             ])))
     return fig
 
-def arima_plot(df,predict_future): #predict_on_test
+def arima_plot(df,predict_future): 
   fig = go.Figure()
   fig.add_trace(go.Scatter(x=df.index, y=df['new_cases_rolling7'],
                       mode='lines',
                       name='Actual data'))
-#   fig.add_trace(go.Scatter(x=train.index, y=train.new_cases_rolling7,
-#                       mode='lines',
-#                       name='train'))
-#   fig.add_trace(go.Scatter(x=test.index, y=test.new_cases_rolling7,
-#                       mode='lines',
-#                       name='test'))
-#   fig.add_trace(go.Scatter(x=test.index, y=predict_on_test['pred'],
-#                       mode='lines',
-#                       name='predict on test set'))
   fig.add_trace(go.Scatter(x=predict_future.index, y=predict_future['pred'],
                       mode='lines',
                       name='predict future'))
@@ -155,11 +136,6 @@ def arima_plot(df,predict_future): #predict_on_test
   fig.add_trace(go.Scatter(x=predict_future.index, y=predict_future['upper'],
                       mode='lines',
                       name='Upper bounds of the 95% confidence interval'))
-#   fig.add_vrect(
-#       x0=test.index[0], x1=test.index[-1],
-#       fillcolor="LightPink", opacity=0.5,
-#       layer="below", line_width=0,
-#       )
   fig.add_vrect(
       x0=predict_future.index[0], x1=predict_future.index[-1],
       fillcolor="Yellow", opacity=0.5,
@@ -183,15 +159,6 @@ def prophet_plot(prophet_df,predict_future):
   fig.add_trace(go.Scatter(x=prophet_df.ds, y=prophet_df['y'],
                       mode='lines',
                       name='Actual data'))
-#   fig.add_trace(go.Scatter(x=train.ds, y=train.y,
-#                       mode='lines',
-#                       name='train'))
-#   fig.add_trace(go.Scatter(x=test.ds, y=test.y,
-#                       mode='lines',
-#                       name='test'))
-#   fig.add_trace(go.Scatter(x=test.ds, y=predict_on_test['yhat'],
-#                       mode='lines',
-#                       name='predict on test set'))
   fig.add_trace(go.Scatter(x=predict_future.index, y=predict_future['yhat'],
                       mode='lines',
                       name='Predict future'))
@@ -201,11 +168,6 @@ def prophet_plot(prophet_df,predict_future):
   fig.add_trace(go.Scatter(x=predict_future.index, y=predict_future['yhat_upper'],
                       mode='lines',
                       name='Upper bounds of the 95% confidence interval'))
-#   fig.add_vrect(
-#       x0=test.ds.iloc[0], x1=test.ds.iloc[-1],
-#       fillcolor="LightPink", opacity=0.5,
-#       layer="below", line_width=0,
-#       )
   fig.add_vrect(
       x0=predict_future.index[0], x1=predict_future.index[-1],
       fillcolor="Yellow", opacity=0.5,
@@ -226,7 +188,7 @@ def prophet_plot(prophet_df,predict_future):
 
 
 # convert an array of values into a dataset matrix
-def create_dataset(data, look_back=1):
+def create_dataset(data, look_back=14):
     dataX, dataY = [], []
     i_range = len(data) - look_back - 1
     print(i_range)
@@ -236,6 +198,13 @@ def create_dataset(data, look_back=1):
     
     return np.array(dataX), np.array(dataY)
 
+def create_dataset2(dataset, time_step=14):
+	dataX, dataY = [], []
+	for i in range(len(dataset)-time_step-1):  
+		a = dataset[i:(i+time_step), 0]   
+		dataX.append(a)
+		dataY.append(dataset[i + time_step, 0])
+	return np.array(dataX), np.array(dataY)
 
 def split(df):
   train = df[:int(0.8*(len(df)))]
@@ -249,17 +218,29 @@ def get_mape(var1, var2):
 
 def simple_lstm(n_steps,n_features):
     model = Sequential()
-    model.add(LSTM(64, activation='relu', input_shape=(n_steps, n_features)))
+    model.add(LSTM(448, activation='relu', input_shape=(n_steps, n_features)))
+    model.add(Dense(7))
+    model.compile(optimizer='adam', loss='mse')
+    return model
+def multi_simple_lstm(n_steps,n_features):
+    model = Sequential()
+    model.add(GRU(320, activation='relu', input_shape=(n_steps, n_features)))
+    model.add(Dropout(0.5))
     model.add(Dense(7))
     model.compile(optimizer='adam', loss='mse')
     return model
 
 def simple_gru(n_steps,n_features):
     model = Sequential()
-    model.add(GRU(128, activation='relu', input_shape=(n_steps, n_features)))
-    model.add(Dense(64))
-    model.add(Dense(128))
-    model.add(Dense(64))
+    model.add(GRU(64, activation='relu', input_shape=(n_steps, n_features)))
+    model.add(Dense(7))
+    model.compile(optimizer='adam', loss='mse')
+    return model
+
+def multi_simple_gru(n_steps,n_features):
+    model = Sequential()
+    model.add(GRU(192, activation='relu', input_shape=(n_steps, n_features)))
+    model.add(Dropout(0.5))
     model.add(Dense(7))
     model.compile(optimizer='adam', loss='mse')
     return model
@@ -280,13 +261,29 @@ def split_sequence(sequence, n_steps_in, n_steps_out):
     y.append(seq_y)
   return np.array(X), np.array(y)
 
+def split_sequence2(sequence1,sequence2, n_steps_in, n_steps_out):
+  X, y = list(), list()
+  for i in range(len(sequence1)):
+    # find the end of this pattern
+    end_ix = i + n_steps_in
+    out_end_ix = end_ix + n_steps_out
+    # check if we are beyond the sequence
+    if out_end_ix > len(sequence1):
+      break
+    # gather input and output parts of the pattern
+    seq_x, seq_y = sequence1[i:end_ix], sequence2[end_ix:out_end_ix]
+    X.append(seq_x)
+    y.append(seq_y)
+  return np.array(X), np.array(y)
+
+@st.cache(suppress_st_warning=True)
 def lstm_uni_model(data,n_periods,model_type):  #preprocess(df['new_cases'],df['rolling7']])
     n_features = 1
     n_epochs=50
     scaler = MinMaxScaler()
     data_scaled = scaler.fit_transform(data.dropna().values.reshape(-1,1))
     # choose a number of time steps
-    n_steps_in, n_steps_out = 14, 1
+    n_steps_in, n_steps_out = 14, 7
     # split into samples
     X, y = split_sequence(data_scaled, n_steps_in, n_steps_out)
     X_train, X_test = split(X)
@@ -326,169 +323,149 @@ def lstm_uni_model(data,n_periods,model_type):  #preprocess(df['new_cases'],df['
             i=i+1
     lst_output_inv = scaler.inverse_transform(lst_output)
     pred_sum7 = []
-    for i in range(7):
+    for i in range(n_periods):
         temp = lst_output_inv[i].sum()
         pred_sum7.append(temp)
     return pred_sum7
 
-  
+   
 def future_exog(col,df,n_periods):
     n_steps=14
-    scaler=MinMaxScaler(feature_range=(0,1))
-    scaled=scaler.fit_transform(np.array(df[col]).reshape(-1,1))
+    scaler_exog=MinMaxScaler(feature_range=(0,1))
+    scaled=scaler_exog.fit_transform(np.array(df[col]).reshape(-1,1))
     train,test=split(scaled)
-    # reshape into X=t,t+1,t+2,t+3 and Y=t+4
-    X_train, y_train = create_dataset(train,n_steps)
-    X_test, y_test = create_dataset(test,n_steps)
-    # reshape input to be [samples, time steps, features] which is required for LSTM
-    X_train =X_train.reshape(X_train.shape[0],X_train.shape[1] , 1)
-    X_test = X_test.reshape(X_test.shape[0],X_test.shape[1] , 1)
-    params = (n_steps,1)
-    model = simple_lstm(*params)
+    X, y = create_dataset2(scaled)
+    X_train, X_test = split(X)
+    y_train, y_test = split(y)
+    X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
+    X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
+    model = Sequential()
+    model.add(LSTM(128,activation='relu', input_shape=(n_steps, 1)))
+    model.add(Dense(128))
+    model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mse')
     model.fit(X_train,y_train,validation_data=(X_test,y_test),epochs=50,batch_size=32,verbose=0,shuffle=False)
-    lookback = len(test) - 14
-    x_input=test[lookback:].reshape(1,-1)
+    x_input=X_test[-1].reshape(1,-1)
     temp_input=list(x_input)
     temp_input=temp_input[0].tolist()
     x_input=np.array(temp_input)
 
     lst_output=[]
     i=0
-    while(i<n_periods):
+    while(i<n_periods+1):
         
-        if(len(temp_input)>14):
+        if(len(temp_input)>n_steps):
             temp_input = temp_input[1:]
-            print('before',type(temp_input))
             x_input=np.array(temp_input)
-            print('after',type(temp_input))
-            print("{} day input {}".format(i,x_input))
             x_input=x_input.reshape(1,-1)
             x_input = x_input.reshape((1, n_steps, 1))
-            #print(x_input)
-            model = simple_lstm(n_steps,1)
-            history = model.fit(X_train,y_train,validation_data=(X_test,y_test),epochs=50,batch_size=32,verbose=0,shuffle=False)
             yhat = model.predict(x_input, verbose=0)
-            print("{} day output {}".format(i,yhat))
             temp_input.extend(yhat[0].tolist())
             temp_input=temp_input[1:]
-            #print(temp_input)
             lst_output.extend(yhat.tolist())
             i=i+1
-            print('i=',i)
         else:
             x_input = x_input.reshape((1, n_steps,1))
-            model = simple_lstm(n_steps,1)
-            history = model.fit(X_train,y_train,validation_data=(X_test,y_test),epochs=50,batch_size=32,verbose=0,shuffle=False)
             yhat = model.predict(x_input, verbose=0)
-            print(yhat[0])
             temp_input.extend(yhat[0].tolist())
-            print(len(temp_input))
             lst_output.extend(yhat.tolist())
             i=i+1
-            print('i=',i)
 
-    lst_output_scaled = scaler.inverse_transform(lst_output)
+
+    lst_output_scaled = scaler_exog.inverse_transform(lst_output)
 
     return lst_output_scaled
 
 
-@st.cache(suppress_st_warning=True)
-def lstm_multi_model(data,n_periods,future_df):  #preprocess(df['new_cases'],df['rolling7']])
-    n_features = 5
+def lstm_multi_model(df,n_periods,future_df): 
     n_epochs=50
-    #prepare future df
-    future_scaler=MinMaxScaler()
-    future_df_scaled = future_scaler.fit_transform(future_df.values)
+    n_features = 4
+    n_steps = 14
+    x_scaler = MinMaxScaler()
+    y_scaler = MinMaxScaler()
+    dataX = x_scaler.fit_transform(df[['new_deaths','full_vac', 'resident_gg','new_cases']])
+    dataY = y_scaler.fit_transform(df[['new_cases']])
+    future_df_scaled = x_scaler.transform(future_df.values)
     future_input = np.array(future_df_scaled)
-    #prepare main df
-    scaler = MinMaxScaler()
-    data_scaled = scaler.fit_transform(data.values)
-    train,test = split(data_scaled)
-    # st.write(train,test)
-    # reshape into X=t,t+1,t+2,t+3 and Y=t+4
-    X_train, y_train = create_dataset(train,14)
-    X_test, y_test = create_dataset(test,14)
+    X,y =split_sequence2(dataX,dataY,14,7)
+    X_train, X_test = split(X)
+    y_train, y_test = split(y)
     X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], n_features)
     X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], n_features)
-    st.write(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
-    best_rmse = np.inf
-    best_mape = np.inf
-    best_lstm_model=None
-    n_steps = 14
-    params = (n_steps,n_features)
-    model = simple_lstm(*params)
+    model = multi_simple_lstm(n_steps,n_features)
     model.fit(X_train, y_train,validation_data=(X_test,y_test), epochs=n_epochs,shuffle=False, verbose=0)
-    #one step prediction on test set
-    y_pred=[]
-    for ix in range(y_test.shape[0]): 
-        x_input = X_test[ix]
-        x_input = x_input.reshape((1, n_steps, n_features))
-        yhat = model.predict(x_input, verbose=0)
-        y_pred.extend(yhat[0])
-        
-    y_test_inv = scaler.inverse_transform(y_test)
-    y_test_inv = y_test_inv[:,-1]
-    x_test = X_test.reshape(X_test.shape[0],X_test.shape[2]*X_test.shape[1])
-    a = np.concatenate((x_test[:,-4:],np.array(y_pred).reshape(-1,1)),axis=1)
-    a = scaler.inverse_transform(a)
-    y_pred_inv = a[:,-1]
-
-    mape = get_mape(y_test_inv, y_pred_inv)
-    st.write("Simple_lstm -  MAPE = {:4.4f}".format(mape))
-    
-    fig = plt.figure(figsize=(16,7))
-    plt.plot(y_pred_inv,label='Pred')
-    plt.plot(y_test_inv,label='Actual')
-    plt.legend()
-    st.write(fig)
-        
-    #forecast future
-    lookback = len(test) - 14
-    x_input=test[lookback:]
+    x_input = np.concatenate([X_test[-1][1:],[future_input[0]]])
     temp_input=list(x_input)
-    x_input=np.array(temp_input) 
+    x_input=np.array(temp_input)
+    future_input = future_input[1:]
     lst_output=[]
-    n_steps=14
     i=0
     while(i<n_periods):
         
-        if(len(temp_input)>14):
-            #print(temp_input)
+        if(len(temp_input)>n_steps):
             temp_input = temp_input[1:]
-            print('before',type(temp_input))
             x_input=np.array(temp_input)
-            print('after',type(x_input))
-            print("{} day input {}".format(i,x_input))
-            print('x_input',x_input)
-            # x_input=x_input.reshape(1,-1)
             x_input = x_input.reshape((1, n_steps, n_features))
-            #print(x_input)
-            model = simple_lstm(n_steps,n_features)
-            history = model.fit(X_train, y_train, validation_data=(X_test,y_test), epochs = 50, verbose=0, shuffle=False)
             yhat = model.predict(x_input, verbose=0)
-            print("{} day output {}".format(i,yhat))
             temp_input.append(future_input[i])
-            # temp_input=temp_input[1:]
-            #print(temp_input)
+            temp_input=temp_input[1:]
             lst_output.extend(yhat.tolist())
             i=i+1
         else:
             x_input = x_input.reshape((1, n_steps,n_features))
-            model = simple_lstm(n_steps,n_features)
-            history = model.fit(X_train, y_train, validation_data=(X_test,y_test), epochs = 50, verbose=0, shuffle=False)
-            yhat = model.predict(x_input, verbose=0)
-            print(yhat[0])
+            yhat = model.predict(x_input, verbose=0) 
             temp_input.append(future_input[i])
-            print('i',i)
-            print('len(temp_input):',len(temp_input))
             lst_output.extend(yhat.tolist())
             i=i+1
-    #inverse future prediction to normal scale
-    future_temp = np.hstack((np.delete(future_df_scaled,-1,axis=1),lst_output))
-    future_temp = future_scaler.inverse_transform(future_temp)
-    lst_output_inv = future_temp[:,-1]
+            
+    lst_output_inv = y_scaler.inverse_transform(lst_output)
+    
+    return lst_output_inv 
+
+def gru_multi_model(df,n_periods,future_df): 
+    n_epochs=50
+    n_features = 4
+    n_steps = 14
+    x_scaler = MinMaxScaler()
+    y_scaler = MinMaxScaler()
+    dataX = x_scaler.fit_transform(df[['new_deaths','full_vac', 'resident_gg','new_cases']])
+    dataY = y_scaler.fit_transform(df[['new_cases']])
+    future_df_scaled = x_scaler.transform(future_df.values)
+    future_input = np.array(future_df_scaled)
+    X,y =split_sequence2(dataX,dataY,14,7)
+    X_train, X_test = split(X)
+    y_train, y_test = split(y)
+    X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], n_features)
+    X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], n_features)
+    model = multi_simple_gru(n_steps,n_features)
+    model.fit(X_train, y_train,validation_data=(X_test,y_test), epochs=n_epochs,shuffle=False, verbose=0)
+    x_input = np.concatenate([X_test[-1][1:],[future_input[0]]])
+    temp_input=list(x_input)
+    x_input=np.array(temp_input)
+    future_input = future_input[1:]
+    lst_output=[]
+    i=0
+    while(i<n_periods):
         
-    return y_pred_inv,lst_output_inv
+        if(len(temp_input)>n_steps):
+            temp_input = temp_input[1:]
+            x_input=np.array(temp_input)
+            x_input = x_input.reshape((1, n_steps, n_features))
+            yhat = model.predict(x_input, verbose=0)
+            temp_input.append(future_input[i])
+            temp_input=temp_input[1:]
+            lst_output.extend(yhat.tolist())
+            i=i+1
+        else:
+            x_input = x_input.reshape((1, n_steps,n_features))
+            yhat = model.predict(x_input, verbose=0) 
+            temp_input.append(future_input[i])
+            lst_output.extend(yhat.tolist())
+            i=i+1
+            
+    lst_output_inv = y_scaler.inverse_transform(lst_output)
+    
+    return lst_output_inv 
 
 
 
